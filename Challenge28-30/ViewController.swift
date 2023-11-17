c83fd2d5da0037f9f7ff1f1e79cacbbf062d6262
vasko34 <vasko_botsev_34@abv.bg>
2023-11-17 18:43:44 +0200

    Initial Commit

---
+++ Challenge28-30/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challenge28-30
+//
+//  Created by User on 17.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
